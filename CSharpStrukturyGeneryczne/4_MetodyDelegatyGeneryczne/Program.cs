fccbaab8e5f36783a8d23aac30264a0148cd8472
Patryk Święcicki <patswiecicki@gmail.com>
2020-08-18 21:58:11 +0200

    Info o różnych typach delegatów, do tego mikro wstęp do wyrażeń lambda i Linq

--- CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
+++ CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,23 +9,37 @@ namespace _4_MetodyDelegatyGeneryczne
 {
     class Program
     {
-        static void CW<T>(T data)
-        {
-            Console.WriteLine(data);
-        }
-
         static void Main()
         {
-            Action<double> print = CW;
-
-
             var circularBuffer = new CircularQueue<double>();
             AddDataToBuffer(circularBuffer);
 
-            //var circularBufferToInt = circularBuffer.ItemTo<double, int>();
+            //Action to delegat który nic nie zwraca, i przyjmuje ilosć parametrów od 0 do 16.
+            Action<double> print = x => Console.WriteLine(x);
+            Action<bool> printBool = x => Console.WriteLine(x);
+            Action<int, int, int, int, int, int, int, int> lambda = (a, b, c, d, e, f, g, h) => Console.WriteLine($"Ciag Bonifacego: {a}, {b}, {c}, {d}, {e}, {f}, {g}, {h}");
+            lambda(1, 1, 2, 3, 5, 8, 13, 21);
 
+            //Func to delegat który zawsze ma jedną wartość wyjściową, oraz od 0 do 16 parametrów wejściowych.
+            Func<double, double> powOf2 = x =>  x * x;
+            Func<int, double, double> multiplication = (x, y) => x * y;
+            print(powOf2(5));
+            print(multiplication(3, 4));
 
-            circularBuffer.Print(print);
+            //Predicate to delegat, który przyjmuje zawsze jedną wartość i zwraca wartość logiczną.
+            Predicate<double> isPositiveNum = x => x > 0;
+            printBool(isPositiveNum(-5));
+
+            //Converter przyjmuje jeden parametr na wejściu i zwraca inną wartość 
+            Converter<double, DateTime> doubleToDateTime = d => new DateTime(2000,1,1).AddDays(d);
+            var circularBufferToDateTime = circularBuffer.ItemTo(doubleToDateTime);
+
+            //można skrócić, do:
+            //var circularBufferToDateTime = circularBuffer.ItemTo(d => new DateTime(2000,1,1).AddDays(d));
+            foreach (var item in circularBufferToDateTime)
+            {
+                Console.WriteLine(item);
+            }
 
             ReadDataFromBuffer(circularBuffer);
         }
--- CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/QueueExtenstions.cs
+++ CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/QueueExtenstions.cs
@@ -2,21 +2,22 @@
 using System.ComponentModel;
 using System;
 using System.Runtime.InteropServices;
+using System.Linq;
 
 namespace _4_MetodyDelegatyGeneryczne
-{
-    public delegate void Printer<T>(T data);
-
+{ 
     public static class QueueExtenstions
     {
-        public static IEnumerable<Tout> ItemTo<T, Tout>(this IQueue<T> queue)
+        public static IEnumerable<Tout> ItemTo<T, Tout>(this IQueue<T> queue, Converter<T, Tout> converter)
         {
-            var converter = TypeDescriptor.GetConverter(typeof(T));
-            foreach (var item in queue)
+            /*foreach (var item in queue)
             {
-                var wynik = converter.ConvertTo(item, typeof(Tout));
-                yield return (Tout)wynik;
-            }
+                var wynik = converter(item);
+                yield return wynik;
+            }*/
+            //Można skrócić do: 
+            return queue.Select(item => converter(item));
+            //Jest to metoda z Linq, o czym później muszę doczytać
         }
         
         public static void Print<T>(this IQueue<T> queue, Action<T> print)
